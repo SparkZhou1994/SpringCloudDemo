89508fe7edaf59cc9a1e800d715c8895d5bcaebf
SparkZhou <zhoujuhui@qq.com>
2018-11-28 11:54:57 +0800

    one eureka,one provider,one invoker

---
+++ provider1/src/main/java/spark/provider1/entity/Person.java
@@ -0,0 +1,46 @@
+package spark.provider1.entity;
+
+/**
+ * @ClassName Person
+ * @Description TODO
+ * @Author Spark
+ * @Date 11/28/2018 11:21 AM
+ * @Version 1.0
+ **/
+public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person(){}
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
