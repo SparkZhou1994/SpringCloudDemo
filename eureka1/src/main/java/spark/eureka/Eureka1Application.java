6b654e0a758e6792ebaf88f686150d0b90d84f06
SparkZhou <zhoujuhui@qq.com>
2019-01-08 17:11:00 +0800

    alter the pom.xml

--- eureka1/src/main/java/spark/eureka/Eureka1Application.java
+++ eureka1/src/main/java/spark/eureka/Eureka1Application.java
@@ -2,12 +2,10 @@ package spark.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableConfigServer
 public class Eureka1Application {
 
     public static void main(String[] args) {
